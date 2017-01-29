6f53f793bc50e96d141cebeb7ecacb08d5986fc1
Fernando Paredes <nano@fdp.io>
2017-01-29 15:15:09 -0800

    [raindrops] Factors of 5 should return Plang

--- raindrops/src/lib.rs
+++ raindrops/src/lib.rs
@@ -3,9 +3,11 @@ pub fn raindrops(num: i32) -> String {
     // - If the number has 5 as a factor, output 'Plang'.
     // - If the number has 7 as a factor, output 'Plong'.
     let factor_of_three = |x: i32| x % 3 == 0;
+    let factor_of_five = |x: i32| x % 5 == 0;
 
     let text = match num {
         x if factor_of_three(x) => "Pling".into(),
+        x if factor_of_five(x) => "Plang".into(),
         _ => format!("{}", num)
     };
 
--- raindrops/tests/raindrops.rs
+++ raindrops/tests/raindrops.rs
@@ -7,7 +7,6 @@ fn test_1() { assert_eq!("1", raindrops::raindrops(1)); }
 fn test_3() { assert_eq!("Pling", raindrops::raindrops(3)); }
 
 #[test]
-#[ignore]
 fn test_5() { assert_eq!("Plang", raindrops::raindrops(5)); }
 
 #[test]
@@ -15,15 +14,12 @@ fn test_5() { assert_eq!("Plang", raindrops::raindrops(5)); }
 fn test_7() { assert_eq!("Plong", raindrops::raindrops(7)); }
 
 #[test]
-#[ignore]
 fn test_6() { assert_eq!("Pling", raindrops::raindrops(6)); }
 
 #[test]
-#[ignore]
 fn test_9() { assert_eq!("Pling", raindrops::raindrops(9)); }
 
 #[test]
-#[ignore]
 fn test_10() { assert_eq!("Plang", raindrops::raindrops(10)); }
 
 #[test]
@@ -39,7 +35,6 @@ fn test_15() { assert_eq!("PlingPlang", raindrops::raindrops(15)); }
 fn test_21() { assert_eq!("PlingPlong", raindrops::raindrops(21)); }
 
 #[test]
-#[ignore]
 fn test_25() { assert_eq!("Plang", raindrops::raindrops(25)); }
 
 #[test]
@@ -51,7 +46,6 @@ fn test_35() { assert_eq!("PlangPlong", raindrops::raindrops(35)); }
 fn test_49() { assert_eq!("Plong", raindrops::raindrops(49)); }
 
 #[test]
-#[ignore]
 fn test_52() { assert_eq!("52", raindrops::raindrops(52)); }
 
 #[test]
@@ -59,5 +53,4 @@ fn test_52() { assert_eq!("52", raindrops::raindrops(52)); }
 fn test_105() { assert_eq!("PlingPlangPlong", raindrops::raindrops(105)); }
 
 #[test]
-#[ignore]
 fn test_12121() { assert_eq!("12121", raindrops::raindrops(12121)); }
