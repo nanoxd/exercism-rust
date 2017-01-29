621b0e7b7fb432988957723a5ae39dbbd70572b5
Fernando Paredes <nano@fdp.io>
2017-01-29 11:01:58 -0800

    [gigasecond] Add gigasecond to time

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -1,6 +1,10 @@
 extern crate chrono;
 use chrono::*;
 
+const GIGASECOND: i64 = 1_000_000_000;
+
 pub fn after(date: DateTime<UTC>) -> DateTime<UTC> {
-    date
+    let dur = Duration::seconds(GIGASECOND);
+
+    date.checked_add(dur).expect("Invalid duration")
 }
