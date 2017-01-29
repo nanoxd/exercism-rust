58eba487fea28d28e9393182ddad821be4beb0ed
Fernando Paredes <nano@fdp.io>
2017-01-29 11:11:32 -0800

    [gigasecond] Refactor to generic

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -3,8 +3,6 @@ use chrono::*;
 
 const GIGASECOND: i64 = 1_000_000_000;
 
-pub fn after(date: DateTime<UTC>) -> DateTime<UTC> {
-    let dur = Duration::seconds(GIGASECOND);
-
-    date.checked_add(dur).expect("Invalid duration")
+pub fn after<Tz: TimeZone>(date: DateTime<Tz>) -> DateTime<Tz> {
+    date + Duration::seconds(GIGASECOND)
 }
