3177e0ac1cedb771a65a10d20c5dddde20f1b4d6
Fernando Paredes <nano@fdp.io>
2017-01-29 15:12:20 -0800

    [raindrops] Return Pling if factor of three

--- raindrops/src/lib.rs
+++ raindrops/src/lib.rs
@@ -1,3 +1,13 @@
-pub fn raindrops(num: u16) -> String {
-    "1".into()
+pub fn raindrops(num: i32) -> String {
+    // - If the number has 3 as a factor, output 'Pling'.
+    // - If the number has 5 as a factor, output 'Plang'.
+    // - If the number has 7 as a factor, output 'Plong'.
+    let factor_of_three = |x: i32| x % 3 == 0;
+
+    let text = match num {
+        x if factor_of_three(x) => "Pling".into(),
+        _ => format!("{}", num)
+    };
+
+    text
 }
