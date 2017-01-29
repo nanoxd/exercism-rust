8c5ead59dfc138f42a91d295068ab59252bfc557
Fernando Paredes <nano@fdp.io>
2017-01-29 14:44:32 -0800

    [leap] Return true for years that are divisible by 4

--- leap/src/lib.rs
+++ leap/src/lib.rs
@@ -1,3 +1,7 @@
 pub fn is_leap_year(year: u32) -> bool {
-    true
+    // on every year that is evenly divisible by 4
+    //   except every year that is evenly divisible by 100
+    //     unless the year is also evenly divisible by 400
+
+    year % 4 == 0
 }
