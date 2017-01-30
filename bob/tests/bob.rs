002c5ae275abfe7ebc876df30ff95b582de62714
Fernando Paredes <nano@fdp.io>
2017-01-29 18:03:31 -0800

    [bob] Add in passing tests

--- bob/tests/bob.rs
+++ bob/tests/bob.rs
@@ -11,7 +11,6 @@ fn test_shouting() {
 }
 
 #[test]
-#[ignore]
 fn test_exclaiming() {
     assert_eq!("Whatever.", bob::reply("Let's go make out behind the gym!"));
 }
@@ -23,25 +22,21 @@ fn test_asking() {
 }
 
 #[test]
-#[ignore]
 fn test_shout_numbers() {
     assert_eq!("Whoa, chill out!", bob::reply("1, 2, 3 GO!"));
 }
 
 #[test]
-#[ignore]
 fn test_shout_weird_characters() {
     assert_eq!("Whoa, chill out!", bob::reply("ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!"));
 }
 
 #[test]
-#[ignore]
 fn test_shout_without_punctuation() {
     assert_eq!("Whoa, chill out!", bob::reply("I HATE YOU"));
 }
 
 #[test]
-#[ignore]
 fn test_non_question_with_question_mark() {
     assert_eq!("Whatever.", bob::reply("Ending with ? means a question."));
 }
