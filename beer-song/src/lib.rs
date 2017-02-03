1c5dd912afde33bc83c82728a4a44e7648314b8e
Fernando Paredes <nano@fdp.io>
2017-02-02 19:30:21 -0800

    [beer_song] Pass first test

---
+++ beer-song/src/lib.rs
@@ -0,0 +1,7 @@
+pub fn verse(num: u8) -> String {
+    "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n".into()
+}
+
+pub fn sing(from: u8, to: u8) -> String {
+    "".into()
+}
