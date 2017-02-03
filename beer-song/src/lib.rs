0b545350ef184e3741431bf422164e84f2f9f2c1
Fernando Paredes <nano@fdp.io>
2017-02-02 19:32:07 -0800

    [beer_song] Pass second test
    TDD ¯\_(ツ)_/¯

--- beer-song/src/lib.rs
+++ beer-song/src/lib.rs
@@ -1,5 +1,9 @@
 pub fn verse(num: u8) -> String {
-    "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n".into()
+    if num == 0 {
+        "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n".into()
+    } else {
+        "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n".into()
+    }
 }
 
 pub fn sing(from: u8, to: u8) -> String {
